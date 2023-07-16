7a6bf58f7981d03999b87e2a10d21aceee7329d5
gyaneshwarprasad <104627641+gyaneshwarprasad@users.noreply.github.com>
2023-07-16 19:59:31 +0530

    Git commit

--- src/main/java/com/email/api/controller/EmailController.java
+++ src/main/java/com/email/api/controller/EmailController.java
@@ -23,6 +23,7 @@ public class EmailController {
     // http://localhost:8080/api/email
 
     // http://localhost:8080/api/email
+    // http://localhost:8080/api/email
 
     @PostMapping
     public void sendEmail(@RequestBody EmailRequest emailRequest) {
