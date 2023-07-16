b7bf50d19f27557ead745fab17d6088385e1a2b6
gyaneshwarprasad <104627641+gyaneshwarprasad@users.noreply.github.com>
2023-07-16 14:02:40 +0530

    Initial commit

---
+++ src/main/java/com/email/api/controller/EmailController.java
@@ -0,0 +1,27 @@
+package com.email.api.controller;
+
+import com.email.api.payload.EmailRequest;
+import com.email.api.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/email")
+public class EmailController {
+    private final EmailService emailService;
+
+    @Autowired
+    public EmailController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @PostMapping
+    public void sendEmail(@RequestBody EmailRequest emailRequest) {
+        // Validate and process the email request as needed
+        emailService.sendEmail(emailRequest.getTo(), emailRequest.getSubject(), emailRequest.getText());
+    }
+}
+
